eb01c98532dd1822dbdcc442be5d9d08ff90527a
yairelmaliah <yairelmaliah319732@gmail.com>
2021-07-02 06:52:47 -0700

    d

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there
+          Hello there im yair
         </p>
         <a
           className="App-link"
