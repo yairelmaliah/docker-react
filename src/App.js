93dbef93133064360a1d510212e9ea47b412f6fb
docker <yairelmaliah319732@.com>
2021-07-02 14:19:44 +0300

    travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HI THEREEEEE
+          Hello there
         </p>
         <a
           className="App-link"
